da30717d65b0db796192f5ea5407496fdd36dab7
Zogitar <71237626+Zogitar@users.noreply.github.com>
2021-08-27 16:50:20 -0500

    Add files via upload

---
+++ 2.2ingredientadjuster.py
@@ -0,0 +1,18 @@
+clew = .078125
+cs = .09375
+ccf = .0625
+tct = .078125
+tve = .0625
+tae = .015625
+ts = .015625
+
+sersi = input("How many servings would you like to make?")
+ss = int(sersi)
+
+print(clew*ss, "cups large egg whites")
+print(cs*ss, "cups sugar (divided)")
+print(ccf*ss, "cups cake flour")
+print(tct*ss, "teaspoons cream of tartar")
+print(tve*ss, "teaspoons vanilla extract")
+print(tae*ss, "teaspoons almond extract")
+print(ts*ss, "teaspoons salt")
