1ba3859b83972cbcaec3ecf468a02f0dee9decef
Zogitar <71237626+Zogitar@users.noreply.github.com>
2021-09-01 13:59:43 -0500

    Update 2.2ingredientadjuster.py

--- 2.2ingredientadjuster.py
+++ 2.2ingredientadjuster.py
@@ -1,18 +1,19 @@
-clew = .078125
-cs = .09375
-ccf = .0625
-tct = .078125
-tve = .0625
-tae = .015625
-ts = .015625
+# Amount of each ingredient required per serving size:
+clew = 1.25/16
+cs = 1.5/16
+ccf = 1/16
+tct = 1.25/16
+tve = 1/16
+tae = .25/16
+ts = .25/16
 
 sersi = input("How many servings would you like to make?")
 ss = int(sersi)
 
-print(clew*ss, "cups large egg whites")
-print(cs*ss, "cups sugar (divided)")
-print(ccf*ss, "cups cake flour")
-print(tct*ss, "teaspoons cream of tartar")
-print(tve*ss, "teaspoons vanilla extract")
-print(tae*ss, "teaspoons almond extract")
-print(ts*ss, "teaspoons salt")
+print(format(clew*ss, '.3f'), "cups large egg whites")
+print(format(cs*ss, '.3f'), "cups sugar (divided)")
+print(format(ccf*ss, '.3f'), "cups cake flour")
+print(format(tct*ss, '.3f'), "teaspoons cream of tartar")
+print(format(tve*ss, '.3f'), "teaspoons vanilla extract")
+print(format(tae*ss, '.3f'), "teaspoons almond extract")
+print(format(ts*ss, '.3f'), "teaspoons salt")
