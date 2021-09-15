dcec1a9b250fd31aaf9164d4b4e94d6c1adcf562
Zogitar <71237626+Zogitar@users.noreply.github.com>
2021-09-15 14:16:57 -0500

    Update 4.1sales.py

--- 4.1sales.py
+++ 4.1sales.py
@@ -1,19 +1,7 @@
 # Sales Calculator:
-sun = input("What were the total sales on Sunday?")
-mon = input("What were the total sales on Monday?")
-tue = input("What were the total sales on Tuesday?")
-wed = input("What were the total sales on Wednesday?")
-thu = input("What were the total sales on Thursday?")
-fri = input("What were the total sales on Friday?")
-sat = input("What were the total sales on Saturday?")
-summ = float(sun)+float(mon)+float(tue)+float(wed)+float(thu)+float(fri)+float(sat)
-aver = format((float(sun)+float(mon)+float(tue)+float(wed)+float(thu)+float(fri)+float(sat))/7, '.2f')
-print("Your total sales on Sunday were: $", sun)
-print("Your total sales on Monday were: $", mon)
-print("Your total sales on Tuesday were: $", tue)
-print("Your total sales on Wednesday were: $", wed)
-print("Your total sales on Thursday were: $", thu)
-print("Your total sales on Friday were: $", fri)
-print("Your total sales on Saturday were: $", sat)
-print("Your total sales for the week was: $", summ)
-print("Your average total sales per day was: $", aver)
+total = 0
+for day in ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"):
+    sales = float(input("What were the total sales for " + day + ": "))
+    total += sales
+print("The total amount of sales for the week was: " + format(total, ",.2f"))
+print("The average amount of sales per day was: " + format(total/7, ",.2f"))
