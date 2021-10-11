abb9751dd05a794f86d0c38451ecf791a4662a2a
Zogitar <71237626+Zogitar@users.noreply.github.com>
2021-10-11 14:09:31 -0500

    Update 8.1acronymprog.py

--- 8.1acronymprog.py
+++ 8.1acronymprog.py
@@ -1,2 +1,8 @@
 # 8.1 Acronym Coding
 name = input("Input a phrase you would like to turn into an acronym: ")
+namsp = name.split()
+acr = ""
+for word in namsp:
+    acr += word[0]
+acr = acr.upper()
+print(acr)
